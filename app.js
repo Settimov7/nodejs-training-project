f18398c50b0f30371a40104152c44cb4d514a7ec
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-03-09 09:45:32 +0400

    change sending html to rendering pug templates

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRouter);
 
 app.use((request, response) => {
-	response.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+	response.status(404).render('404');
 });
 
 app.listen(3000);
