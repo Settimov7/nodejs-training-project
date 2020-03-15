8460ffc91bdc110352c99024a2033de84ca03950
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-03-15 10:28:35 +0400

    add product model

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,19 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (request, response) => {
 	response.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 };
 
 exports.postAddProduct = (request, response) => {
-	products.push({ title: request.body.title });
+	const product = new Product(request.body.title);
+
+	product.save();
 
 	response.redirect('/');
 };
 
 exports.getProducts = (request, response) => {
+	const products = Product.fetchAll();
+
 	response.render('shop', { prods: products, pageTitle: 'Shop', path: '/' });
 };
