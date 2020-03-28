bd68e5cf55261a4bc5c9c486aea0ff118512faf4
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-03-28 08:35:46 +0400

    create order and order-item models

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -39,13 +41,14 @@ Product.belongsTo(User, {
 	constraints: true,
 	onDelete: 'CASCADE',
 });
-
 User.hasMany(Product);
 User.hasOne(Cart);
-
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize.sync()
 .then(() => User.findByPk(1))
