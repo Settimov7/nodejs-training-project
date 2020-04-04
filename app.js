71f164a726139789b6d95b9c279aee539088cf32
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-04-04 10:23:53 +0400

    add creating user with mongoose

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorsController = require('./controllers/errors');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -15,17 +15,15 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((request, response, next) => {
-// 	User.findById('5e836c811c9d440000a05cb2')
-// 	.then((user) => {
-// 		const { name, email, cart, _id } = user;
-//
-// 		request.user = new User(name, email, cart, _id);
-//
-// 		next();
-// 	})
-// 	.catch((error) => console.log(error));
-// });
+app.use((request, response, next) => {
+	User.findById('5e8826dee499c7011c520f15')
+	.then((user) => {
+		request.user = user;
+
+		next();
+	})
+	.catch((error) => console.log(error));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,6 +32,20 @@ app.use(errorsController.get404);
 
 mongoose.connect('mongodb+srv://prytkov:N6BhxHED5lM0qM2A@cluster0-pui26.mongodb.net/shop?retryWrites=true&w=majority')
 .then(() => {
+	User.findOne().then((user) => {
+		if (!user) {
+			const user = new User({
+				name: 'Name',
+				email: 'name@mail.com',
+				cart: {
+					items: [],
+				},
+			});
+
+			user.save();
+		}
+	});
+
 	app.listen(3000);
 })
 .catch((error) => console.log(error));
--- models/user.js
+++ models/user.js
@@ -1,3 +1,32 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	cart: {
+		items: [
+			{
+				productId: {
+					type: Schema.Types.ObjectId,
+					required: true,
+				},
+				quantity: {
+					type: Number,
+					required: true,
+				}
+			},
+		],
+	},
+});
+
+module.exports = model('User', userSchema);
+
 // const { ObjectId } = require('mongodb');
 // const { getDataBase } = require('../util/database');
 //
