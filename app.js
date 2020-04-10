95530cb258e854d67152fa0591bb0ad385409840
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-04-10 19:39:19 +0400

    add static folder for images

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(multer({ storage: fileStorage, fileFilter }).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(session({
 	secret: 'my secret',
 	resave: false,
